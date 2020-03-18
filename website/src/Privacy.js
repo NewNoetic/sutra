ab9002153ce7a3a90d23b959913f87fa65760e4c
sdgandhi <sdgandhi@users.noreply.github.com>
2020-03-18 15:43:31 -0700

    Update privacy policy.

--- website/src/Privacy.js
+++ website/src/Privacy.js
@@ -9,13 +9,13 @@ class Privacy extends Component {
 					<Col>
 						<h1>Privacy Policy</h1>
 
-						<p>catchup operates the catchup website, which provides the the related app, website and services.</p>
+						<p>Sutra operates the Sutra website, which provides the the related app, website and services.</p>
 
-						<p>This page is used to inform website visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service, the catchup website.</p>
+						<p>This page is used to inform website visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service, the Sutra website.</p>
 
 						<p>If you choose to use our Service, then you agree to the collection and use of information in relation with this policy. The Personal Information that we collect are used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.</p>
 
-						<p>The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at catchup, unless otherwise defined in this Privacy Policy.</p>
+						<p>The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at Sutra, unless otherwise defined in this Privacy Policy.</p>
 
 						<h2>Information Collection and Use</h2>
 
